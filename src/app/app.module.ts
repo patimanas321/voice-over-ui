9fcf0e7ba06019f5d0fb30d632d774b4d9a48027
Manas <patimanas321@yahoo.com>
2018-11-21 22:06:16 +0530

    Login page view implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ApplicationLogoComponent } from './components/app-logo/app-logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ApplicationLogoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/app-logo/app-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './app-logo.component.html',
+  styleUrls: ['./app-logo.component.scss']
+})
+export class ApplicationLogoComponent {
+
+  
+}
